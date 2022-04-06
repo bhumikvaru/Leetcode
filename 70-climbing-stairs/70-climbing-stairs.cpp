c5907b026fa4a82916b5abdc2886d3784f526307
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2022-04-06 13:30:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (55.55%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int prev1=1, prev2=2;
+        int res;
+        for(int i =3;i<=n;i++)
+        {
+            res = prev1+prev2;
+            prev1=prev2;
+            prev2= res;
+            
+        }
+        return res;
+    }
+};
