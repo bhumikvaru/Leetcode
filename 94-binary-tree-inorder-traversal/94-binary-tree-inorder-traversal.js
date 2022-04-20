9fdf10b5f896bbfe49ae83a802420766b46e1b71
bhumikvaru <91276141+bhumikvaru@users.noreply.github.com>
2022-04-20 15:55:35 +0530

    Time: 72 ms (60.92%), Space: 42 MB (86.16%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -1,17 +1,34 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
 var inorderTraversal = function(root) {
-    if(root === null)
-        return [];
-    return [ ...inorderTraversal(root.left),root.val, ...inorderTraversal(root.right)];
+  const output = [];
+  
+  if (root === null) {
+    return output;
+  }
+  
+  /**
+   * The goal is to maintain a stack of nodes to visit as we traverse 
+   * down the tree. As we traverse down, We go left and push all the 
+   * left nodes first in the stack. Once we reach to the bottom, we 
+   * store the node value and traverse right.
+   *           1
+   *         /   \
+   *        2     3    preorder traversal: 4 -> 2 -> 5 -> 1 -> 6 -> 3
+   *       / \   /     (left -> root -> right)
+   *      4   5 6
+   */
+  const stack = [];
+  let curr = root;
+  
+  while (curr !== null || stack.length !== 0) {
+    if (curr !== null) {
+      stack.push(curr);
+      curr = curr.left;
+    } else {
+      curr = stack.pop();
+      output.push(curr.val);
+      curr = curr.right;  
+    }  
+  }
+  
+  return output;
 };
